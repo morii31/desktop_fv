5fdbbacaa50c36285f2856596977b4d100883151
morii31 <pfla31@gmail.com>
2023-01-19 10:02:37 +0900

    third commit

--- Verilog/ZC706/project_ZC706_jesd/my_adcphy.v
+++ Verilog/ZC706/project_ZC706_jesd/my_adcphy.v
@@ -35,22 +35,22 @@ module my_adcphy(
     input wire DDM_i,
     input wire SYSREFP_i,
     input wire SYSREFM_i,
-    input wire SYNCP_i,
-    input wire SYNCM_i,
+    input wire SYNC_i,
     output wire refclk_p_o,
     output wire refclk_n_o,
     output wire subclk_p_o,
     output wire subclk_n_o,
-    output wire DAP_o,
-    output wire DAM_o,
-    output wire DBP_o,
-    output wire DBM_o,
-    output wire DCP_o,
-    output wire DCM_o,
-    output wire DDP_o,
-    output wire DDM_o,
-    output wire SYSREFP_o,
-    output wire SYSREFM_o,
+    output wire [3:0] DP_o,
+    output wire [3:0] DM_o,
+//    output wire DAP_o,
+//    output wire DAM_o,
+//    output wire DBP_o,
+//    output wire DBM_o,
+//    output wire DCP_o,
+//    output wire DCM_o,
+//    output wire DDP_o,
+//    output wire DDM_o,
+    output wire SYSREF_o,
     output wire SYNCP_o,
     output wire SYNCM_o
     );
@@ -58,17 +58,29 @@ assign refclk_p_o = refclk_p_i;
 assign refclk_n_o = refclk_n_i;
 assign subclk_p_o = subclk_p_i;
 assign subclk_n_o = subclk_n_i;
-assign DAP_o = DAP_i;
-assign DAM_o = DAM_i;
-assign DBP_o = DBP_i;
-assign DBM_o = DBM_i;
-assign DCP_o = DCP_i;
-assign DCM_o = DCM_i;
-assign DDP_o = DDP_i;
-assign DDM_o = DDM_i;
-assign SYSREFP_o = SYSREFP_i;
-assign SYSREFM_o = SYSREFM_i;
-assign SYNCP_o = SYNCP_i;
-assign SYNCM_o = SYNCM_i;
+assign DP_o = {DAP_i, DBP_i, DCP_i, DDP_i};
+assign DM_o = {DAM_i, DBM_i, DCM_i, DDM_i};
+//assign DAP_o = DAP_i;
+//assign DAM_o = DAM_i;
+//assign DBP_o = DBP_i;
+//assign DBM_o = DBM_i;
+//assign DCP_o = DCP_i;
+//assign DCM_o = DCM_i;
+//assign DDP_o = DDP_i;
+//assign DDM_o = DDM_i;
+//assign SYSREFP_o = SYSREFP_i;
+//assign SYSREFM_o = SYSREFM_i;
+//assign SYNCP_o = SYNCP_i;
+//assign SYNCM_o = SYNCM_i;
 
+IBUFDS ibufds_sysref(
+    .O (SYSREF_o),
+    .I (SYSREFP_i),
+    .IB(SYSREFM_i)
+);
+OBUFDS obufds_sync(
+    .O (SYNCP_o),
+    .OB(SYNCM_o),
+    .I (SYNC_i)
+);
 endmodule
